9dd15fc86b9c5a391b9a828a83da32a102d363f1
Alexandre Sinn <alexandresinn@gmail.com>
2020-08-14 16:49:34 +0200

    active Storage

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,6 +7,10 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
@@ -14,6 +18,12 @@ class CocktailsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path, :notice => "Your cocktail has been deleted"
   end
 
   private
